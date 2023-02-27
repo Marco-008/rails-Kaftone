b17db3c98da937ae4b069b2a1ecf4970f145cba0
sanaa semlali <sanaa.semlali@gmail.com>
2023-02-27 17:03:10 +0100

    changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+items.destroy_all
+
+10.times do
+  Item.create({user_id: 1, title: "caftan YSL", description: "chic", price: "1000", location: "casa", picture:"", color: ""})
+end
