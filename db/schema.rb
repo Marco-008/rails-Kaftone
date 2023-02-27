38073322548a09c653b3f5c57be3206f5806657c
polo Garniga <kenpococo@gmail.com>
2023-02-27 16:58:50 +0100

    Tables,RootforIndex&Show,Models,Controllers,Views

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_27_123649) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_27_145141) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.float "price"
+    t.string "location"
+    t.string "picture"
+    t.string "review"
+    t.string "color"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +40,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_27_123649) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Cleaning database..."
+Item.destroy_all
+
+puts "Creating items..."
+kaftan1 = {user_id: 1, title: "Kaftan YSL 1", description: "Lorem Ipsum",
+  price: "1260 mad per day", location: "Casablanca", picture: "", review: "", color: "red" }
+kaftan2 = {user_id: 1, title: "Kaftan YSL 2", description: "Lorem Ipsum",
+  price: "1260 mad per day", location: "Casablanca", picture: "", review: "", color: "black" }
+kaftan3 ={user_id: 1, title: "Kaftan YSL 3", description: "Lorem Ipsum",
+  price: "1260 mad per day", location: "Casablanca", picture: "", review: "", color: "yellow" }
+
+[kaftan1, kaftan2, kaftan3].each do |attributes|
+  kaftan = Item.create!(attributes)
+  puts "Created #{kaftan.title}"
+end
+puts "Finished!"
