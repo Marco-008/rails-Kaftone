88a03401be18224f854b1fbba7b414f82cef7dc5
khalidltf <khalidlotfi2005@gmail.com>
2023-02-28 17:07:45 +0100

    delete photo

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   get 'items/index'
   devise_for :users
-  resources:items, only: [:index, :show]
+  resources :items, only: [:index, :show, :new, :create]
   root to: "items#index"
-
 end
