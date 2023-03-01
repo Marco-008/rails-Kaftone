360f29cdc0b5d1bd8b63a8fe1d07776660abc9da
sanaa semlali <sanaa.semlali@gmail.com>
2023-03-01 11:01:17 +0100

    added destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :items, only: [:index, :show, :new, :create]
-  root to: "items#index"  
+  resources :items, only: [:index, :show, :new, :create, :destroy]
+  root to: "items#index"
   get 'search', to: "items#search"
 end
