cefbd0ba08e36edfc1f7ba94ea02b893ba5560b9
ghizlane ait dadda ali <ghizlane.aitdaddaali94@gmail.com>
2023-03-01 10:36:13 +0100

    added delete caftan

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,41 +1,35 @@
 class ItemsController < ApplicationController
-  def index
-    @items = Item.all
-  end
-
-  def show
-    @item = Item.find(params[:id])
-  end
-
-  def new
-    @item = Item.new
-  end
-
-  def create
-    @item = Item.new(item_params)
-    # @item.user_id = nil
-    @item.user = current_user
-
-    if @item.save!
-      redirect_to item_path(@item)
-    else
-      render :new
+  before_action :set_item, only: [:show, :destroy]
+    def index
+      @items = Item.all
+    end
+    def show
+    end
+    def new
+      @item = Item.new
+    end
+    def create
+      @item = Item.new(item_params)
+      # @item.user_id = nil
+      @item.user = current_user
+      if @item.save!
+        redirect_to item_path(@item)
+      else
+        render :new
+      end
+    end
+    def destroy
+      @item.destroy
+      redirect_to items_path, status: :see_other
+    end
+    def search
+      @items = Item.where("LOWER(title) LIKE ?", "%" + params[:q].downcase + "%")
+    end
+    private
+    def item_params
+      params.require(:item).permit(:title, :description, :price, :location, :color, :photo)
+    end
+    def set_item
+      @item = Item.find(params[:id])
     end
   end
-  
-  def search
-    @items = Item.where("LOWER(title) LIKE ?", "%" + params[:q].downcase + "%")
-  end
-
-  private
-
-  def item_params
-    params.require(:item).permit(:title, :description, :price, :location, :color, :photo)
-  end
-
-  def set_item
-
-  end
-  
-
-end
