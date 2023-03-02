d69bb1e43e1f69d76aa594906645e54b3fa55705
polo Garniga <kenpococo@gmail.com>
2023-03-02 10:31:49 +0100

    fixing the destroy button

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :items
+
   root to: "items#index"
   get 'search', to: "items#search"
+  delete 'items/:id', to: "items#destroy", as: :item_d
 end
