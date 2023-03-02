a9239c105a7b5d0840b0a4ea889672132d171e75
ghizlane ait dadda ali <ghizlane.aitdaddaali94@gmail.com>
2023-03-02 14:33:38 +0100

    added fist and last name to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :first_name, presence:true
+  validates :last_name, presence:true
 end
