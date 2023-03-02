51ed4c272ac6dcfa65f4749227e20187b9dd8d4f
khalidltf <khalidlotfi2005@gmail.com>
2023-03-02 15:01:40 +0100

    fixed log in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,6 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home]
+  skip_before_action :authenticate_user!, only: :home
 
   def home
   end
 end
-
-
